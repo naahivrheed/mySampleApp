658129cabcff7bc1dd31603e4ba9f66340afb6d9
Vihaan Dheer <vihaandheer@gmail.com>
2018-10-25 06:58:12 -0400

    This is with the scroll view.

---
+++ sampleRealApp/UserView/Controllers/NextOneViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NextOneViewController.swift
+//  sampleRealApp
+//
+//  Created by Vihaan Dheer on 10/25/18.
+//  Copyright © 2018 Vihaan Dheer. All rights reserved.
+//
+
+import UIKit
+
+class NextOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ sampleRealApp/UserView/Controllers/ScrollingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollingViewController.swift
+//  sampleRealApp
+//
+//  Created by Vihaan Dheer on 10/25/18.
+//  Copyright © 2018 Vihaan Dheer. All rights reserved.
+//
+
+import UIKit
+
+class ScrollingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
