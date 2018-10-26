6b8f3ff6b3189fc4b9d4275b56f907f64413cbdd
Vihaan Dheer <vihaandheer@gmail.com>
2018-10-25 20:25:51 -0400

    next commit with animations.

--- sampleRealApp/UserView/Controllers/NextOneViewController.swift
+++ sampleRealApp/UserView/Controllers/NextOneViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class NextOneViewController: UIViewController {
 
+    @IBOutlet weak var animatingView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // animatingView.center.y -= view.bounds.height
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.5, delay: 0.0, options: [], animations: {
+            // self.animatingView.center.y += self.view.bounds.height
+            // self.animatingView.backgroundColor = UIColor.red
+            self.animatingView.frame = CGRect(x: 0, y: 252, width: 375, height: 143)
+        }, completion: nil)
+    }
 }
